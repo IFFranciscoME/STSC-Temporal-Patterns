a9a4d91160a50089368d6ee03f735d1a803c277f
IFFranciscoME <if.francisco.me@gmail.com>
2020-02-25 18:42:27 -0600

    avances tabla ANOVA

--- principal.py
+++ principal.py
@@ -9,6 +9,7 @@
 # Importar Precios historicos M1 del 2019
 from datos import df_usdmxn, df_ce
 import funciones as fn
+import numpy as np
 
 # -- -------------------------------------------------------------------- Data generation -- #
 # Importar Calendario Economico
@@ -50,12 +51,56 @@ df_indes.reset_index(inplace=True, drop=True)
 
 # -- construir tabla de indicador_escenario con los candidatos
 param_tab = df_indes
-ind = list(set(df_indes['indicador']))
-esc = ['A', 'B', 'C', 'D']
+indicadores = list(set(df_indes['indicador']))
 
-# -- seleccionar indicador
-df_indes[df_indes['indicador'] == ind[0]]
+# -- escenarios diferentes de 0 para el indicador
+
+# nombre de indicador
+tab_ind = []
+# nombre de escenario
+tab_esc = []
+# observaciones totales
+tab_obs = []
+# cantidad de observaciones por grupo
+tab_grp = []
+# 1: no hay variabilidad (se utiliza indicador-escenario)
+# 0: hay variabilidad (se descarta indicador-escenario)
+tab_anova1 = []
+tab_anova2 = []
+tab_anova3 = []
+tab_anova4 = []
+
+ind = indicadores[3]
+data = df_indes[df_indes['indicador'] == ind]
+esc = list()
+n_esc = list()
+p_esc = list()
+
+if list(data['A'])[0] >= 30:
+    esc.append('A')
+    n_esc.append(list(data['A'])[0])
+    p_esc.append(int(list(data['A'])[0]/3))
+if list(data['B'])[0] >= 30:
+    esc.append('B')
+    n_esc.append(list(data['B'])[0])
+    p_esc.append(int(list(data['B'])[0] / 3))
+if list(data['C'])[0] >= 30:
+    esc.append('C')
+    n_esc.append(list(data['C'])[0])
+    p_esc.append(int(list(data['C'])[0] / 3))
+if list(data['D'])[0] >= 30:
+    esc.append('D')
+    n_esc.append(list(data['D'])[0])
+    p_esc.append(int(list(data['D'])[0] / 3))
+
+if len(esc) != 0:
+    tab_ind.extend([ind]*len(esc))
+    tab_esc.extend(esc)
+    tab_obs.extend(n_esc)
+    tab_grp.extend(p_esc)
+
+
+
 # -- -- encontrar el escenario donde hay mas de 30 observaciones
-
 # -- eligir aleatoriamente la misma cantidad de observaciones para 3 grupos distintos
 # -- Hacer prueba ANOVA a cada indicador_escenario con sus 3 grupos
