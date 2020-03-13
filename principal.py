54d1c7ee939b665cd0c347e3b2ff25681621da4b
IFFranciscoME <if.francisco.me@gmail.com>
2020-03-13 15:00:37 -0600

    imprimir nucleos disponibles

--- principal.py
+++ principal.py
@@ -11,7 +11,6 @@ import multiprocessing as mp
 import funciones as fn
 import pandas as pd
 import time
-import json
 from multiprocessing import cpu_count
 
 import warnings
@@ -19,7 +18,8 @@ warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
 
-    print('Inicio de ejecucion')
+    print(' -- ---------------- Inicio de ejecucion ---------------- --')
+    print('la cantidad de nucleos disponibles es: ' + str(mp.cpu_count()))
 
     # -- ------------------------------------------------------------------- FUNCTION : 1 -- #
     # -- Calcular escenarios para indicadores
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     df_ce = fn.f_escenario(p0_datos=df_ce)
     e_f1 = time.time()
     time_f1 = round(e_f1 - s_f1, 4)
-    # print('f_escenario se tardo: ' + str(time_f1))
+    print('f_escenario se tardo: ' + str(time_f1))
 
     # -- ------------------------------------------------------------------- FUNCTION : 2 -- #
     # -- Calcular las metricas para reacciones del precio
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     df_ce = fn.f_metricas(param_ce=df_ce, param_ph=df_precios)
     e_f2 = time.time()
     time_f2 = round(e_f2 - s_f2, 4)
-    # print('f_metricas se tardo: ' + str(time_f2))
+    print('f_metricas se tardo: ' + str(time_f2))
 
     # -- ------------------------------------------------------ Data exploratory analysis -- #
 
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     df_ind_1 = fn.f_tabla_ind(param_ce=df_ce)
     e_f3 = time.time()
     time_f3 = round(e_f3 - s_f3, 2)
-    # print('f_tabla_ind se tardo: ' + str(time_f3))
+    print('f_tabla_ind se tardo: ' + str(time_f3))
 
     # -- ------------------------------------------------------------------- FUNCTION : 4 -- #
     # -- Seleccionar indicadores y escenarios con observaciones suficientes
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     df_ind_2 = fn.f_seleccion_ind(param_ce=df_ind_1, param_c1=120, param_c2=40)
     e_f4 = time.time()
     time_f4 = round(e_f4 - s_f4, 2)
-    # print('f_seleccion_ind se tardo: ' + str(time_f4))
+    print('f_seleccion_ind se tardo: ' + str(time_f4))
 
     # -- ------------------------------------------------------------------- FUNCTION : 5 -- #
     # -- Construir tabla de anova para seleccionar escenarios candidatos
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     df_ind_3 = fn.f_anova(param_data1=df_ind_2, param_data2=df_ce)
     e_f5 = time.time()
     time_f5 = round(e_f5 - s_f5, 2)
-    # print('f_anova se tardo: ' + str(time_f3))
+    print('f_anova se tardo: ' + str(time_f3))
 
     # -- ------------------------------------------------------------------- FUNCTION : 6 -- #
     # -- Busqueda hacia adelante de patrones en serie de tiempo
