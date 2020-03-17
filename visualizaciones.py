2e6eb512b0ad1083cb8643baab5105282fce13b6
IFFranciscoME <if.francisco.me@gmail.com>
2020-03-17 10:49:02 -0600

    Velas OHLC + 3 lineas

--- visualizaciones.py
+++ visualizaciones.py
@@ -53,3 +53,70 @@ def g_lineas(param_query, param_pattern):
     fig.show()
 
     return fig
+
+
+# -- ------------------------------------------------------- GRÁFICA: velas OHLC Reaccion -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+def g_velas_reaccion(param_timestamp, param_ohlc, param_serie1, param_serie2, param_serie3):
+    """
+    Parameters
+    ----------
+    param_timestamp
+    param_ohlc
+    param_serie1
+    param_serie2
+    param_serie3
+
+    Returns
+    -------
+
+    Debugging
+    ---------
+    param_ohlc = results['ciclo_3'][0]['datos']['ConsumrgyMoM_USA_A_2015-12-15_13:30:00']
+    param_ohlc = param_ohlc['df_serie_q']
+    param_timestamp = param_ohlc['timestamp']
+    param_serie1 = param_ohlc['close']
+    param_serie2 = param_ohlc['mid_hl']
+    param_serie3 = param_ohlc['mid_oc']
+    """
+
+    f_i = param_timestamp[0]
+    yini = param_ohlc['high'][0]
+    yfin = max(param_ohlc['close'])
+    fig = go.Figure(data=[go.Candlestick(name='ohlc', x=param_ohlc['timestamp'],
+                                         open=param_ohlc['open'], high=param_ohlc['high'],
+                                         low=param_ohlc['low'], close=param_ohlc['close'],
+                                         opacity=0.4)])
+
+    fig.add_trace(go.Scatter(x=param_timestamp, y=param_serie1, name='close',
+                             line=dict(color='blue', width=2, dash='dash')))
+    fig.add_trace(go.Scatter(x=param_timestamp, y=param_serie2, name='mid hl',
+                             line=dict(color='red', width=2, dash='dash')))
+    fig.add_trace(go.Scatter(x=param_timestamp, y=param_serie3, name='mid oc',
+                             line=dict(color='green', width=2, dash='dash')))
+
+    lineas = [dict(x0=f_i, x1=f_i, xref='x', y0=yini, y1=yfin, yref='y', type='line',
+                   line=dict(color='red', width=1.5, dash='dashdot'))]
+
+    fig.update_layout(margin=go.layout.Margin(l=50, r=50, b=20, t=50, pad=20),
+                      title=dict(x=0.5, text='Reaccion del precio durante '
+                                             'comunicado de <b> indicador </b>'),
+                      xaxis=dict(title_text='Hora del dia', rangeslider=dict(visible=False)),
+                      yaxis=dict(title_text='Precio del EurUsd'), shapes=lineas,
+                      annotations=[go.layout.Annotation(x=f_i, y=0.95, xref="x", yref="paper",
+                                                        showarrow=False, text="Indicador",
+                                                        font=dict(size=15)),
+                                   go.layout.Annotation(x=f_i, y=0.90, xref="x", yref="paper",
+                                                        showarrow=False, text="Comunicado",
+                                                        font=dict(size=15))])
+
+    # fig.update_layout(legend_orientation="h")
+    fig.update_layout(legend=go.layout.Legend(x=.3, y=-.15, orientation='h',
+                                              font=dict(size=15)))
+    fig.layout.autosize = False
+    fig.layout.width = 840
+    fig.layout.height = 520
+    fig.show()
+
+    return fig
